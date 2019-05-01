91a805782366282391160023a1774d1b2c9990d7
ngrayson <intercomman14@gmail.com>
2019-04-30 17:31:08 -0700

    initial enemy bot manager up

---
+++ Assets/Scripts/BootStrapManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BootStrapManager : MonoBehaviour
+{
+    enum GameState {Title, Play, Loss, Win};
+    GameState currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //load title and set to title
+    }
+
+    void setTitle()
+    {
+    }
+
+    void setPlay()
+    {
+    
+    }
+
+    void setLoss()
+    {
+
+    }
+
+    void setWin()
+    {
+        
+    }
+    void clearScene(Scene targetScene)
+    {
+        // deletes everything in the target scene to unload it
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BotManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BotManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 velocity;
+    void Start()
+    {
+        Random.Range(0.001f,0.07f);
+        velocity = new Vector2(0.05f,0.05f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 botPos = this.transform.position;
+        Vector2 newPos = botPos+velocity;
+        this.transform.position=newPos;
+    }
+}
